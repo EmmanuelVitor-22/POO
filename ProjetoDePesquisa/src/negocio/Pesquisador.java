877e46490058444c70aedcbc3f0ff90489721003
Emmanuel Vitor <emmanuelvitor.ti@gmail.com>
2022-09-20 22:17:22 -0300

    Criação da questão Projeto de Pesquisa, da materia de POO

---
+++ ProjetoDePesquisa/src/negocio/Pesquisador.java
@@ -0,0 +1,11 @@
+package negocio;
+
+public  class Pesquisador {
+    private String nome;
+    private  Pesquisador coordenador;
+    private  Pesquisador pesquisador;
+    private int qtdDePesquisadores;
+
+
+
+}
---
+++ ProjetoDePesquisa/src/negocio/ProjetoDePesquisa.java
@@ -0,0 +1,22 @@
+package negocio;
+
+import java.util.ArrayList;
+
+public class ProjetoDePesquisa {
+
+    ArrayList<ProjetoDePesquisa> projetos;
+
+    //metodo para fazer a inserção do pesquisador
+    public void inserirPesquisador(){
+
+    }
+    //para retornar a quantidade de pesquisadores associados a um pesquisador
+    public void getQtdPesquisadores(){
+
+    }
+//" para retornar o valor a ser pago pelo pesquisador para se inscrever no Projeto
+    public void getValorPago(){
+
+    }
+
+}
